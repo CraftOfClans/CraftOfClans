1a76dafd21ed000412ab59e706910e925c276e09
SecondFlight <qbgeekjtw@gmail.com>
2014-09-21 20:02:19 -0400

    forgot to clean up imports

--- src/com/icraftgames/util/GetTargetBlock.java
+++ src/com/icraftgames/util/GetTargetBlock.java
@@ -1,13 +1,9 @@
 package com.icraftgames.util;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
-import org.bukkit.util.Vector;
 
 public class GetTargetBlock {
 	
